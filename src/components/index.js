2463a8f0fef286498567ab3e569a62f3c1eb1fa4
Pauly-Thomas <samoht.pauly@gmail.com>
2020-11-26 18:43:07 +0100

    delete all

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as Competences } from "./Competences";
-export { default as Formation } from "./Formation";
-export { default as Experiences } from "./Experiences";
